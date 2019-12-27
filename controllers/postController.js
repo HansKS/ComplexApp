5217c32c246783de0ad560be2be52b17ebd79375
Hans <hanskarlssonsalazar@gmail.com>
2019-12-27 16:10:04 -0600

    Third Commit

--- controllers/postController.js
+++ controllers/postController.js
@@ -7,7 +7,7 @@ exports.viewCreateScreen = function(req, res) {
 exports.create = function(req, res){
     let post = new Post(req.body, req.session.user._id)
     post.create().then(function(newId){
-        res.flash("success", "New post successfully created.")
+        req.flash("success", "New post successfully created.")
         req.session.save(()=> res.redirect(`/post/${newId}`))
     }).catch(function(errors){
         errors.forEach(error => req.flash("errors", error))
@@ -24,19 +24,19 @@ exports.viewSingle = async function(req, res){
     }
 }
 
-exports.viewEditScreen = async function(req, res){
-    try{
-        let post = await Post.findSingleById(req.params.id, req.visitorId)
-        if(post.authorId == req.visitorId){
-            res.render("edit-post", {post: post})
-        }else{
-            req.flash("errors", "You do not have permission to perform that action.")
-            req.session.save(() => res.redirect("/"))
-        }
-    }catch{
-        res.render("404")
+exports.viewEditScreen = async function(req, res) {
+    try {
+      let post = await Post.findSingleById(req.params.id, req.visitorId)
+      if (post.isVisitorOwner) {
+        res.render("edit-post", {post: post})
+      } else {
+        req.flash("errors", "You do not have permission to perform that action.")
+        req.session.save(() => res.redirect("/"))
+      }
+    } catch {
+      res.render("404")
     }
-}
+  }
 
 exports.edit = function(req, res){
     let post = new Post(req.body, req.visitorId, req.params.id)
@@ -65,5 +65,15 @@ exports.edit = function(req, res){
         req.session.save(function(){
             res.redirect("/")
         })
+    })
+}
+
+exports.delete = function(req, res){
+    Post.delete(req.params.id, req.visitorId).then(()=>{
+        req.flash("success", "Post successfully deleted.")
+        req.session.save(()=>res.redirect(`/profile/${req.session.user.username}`))
+    }).catch(()=>{
+        req.flash("errors", "You do not have permission to perform that action.")
+        req.session.save(()=> res.redirect('/'))
     })
 }
